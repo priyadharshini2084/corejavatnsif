5517d9a9493656e2014285804bf5380dbcd0bbb0
priyadharshini2084 <priyadhasinipriya@gmail.com>
2025-12-19 02:16:46 +0530

    if and if else

---
+++ tnsif.javaprograms/src/tnsif/javaprograms/basics/IfDemo.java
@@ -0,0 +1,15 @@
+package tnsif.javaprograms.basics;
+
+public class IfDemo {
+	public static void main(String[] args) {
+
+        int x = 6;
+
+        if (x != 5) {
+            System.out.println("Value of x is not 5");
+        } else {
+            System.out.println("Value of x is 5");
+        }
+
+	}
+}
---
+++ tnsif.javaprograms/src/tnsif/javaprograms/basics/IfElseDemo.java
@@ -0,0 +1,17 @@
+package tnsif.javaprograms.basics;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		  int age = 11;
+
+	        if (age >= 18)
+	            System.out.println("You are eligible for voting");
+	        else
+	            System.out.println("Sorry! You need to wait more");
+
+	        System.out.println("if else demonstration");
+
+	}
+
+}
